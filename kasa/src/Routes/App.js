26b72723fb3b784af4670e5420ebbcd59d0ec245
Jeremie Brandely <jeremie.brandely@gmail.com>
2023-07-31 10:59:19 +0200

    deploy site correction

--- kasa/src/Routes/App.js
+++ kasa/src/Routes/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Error from '../pages/Error';
